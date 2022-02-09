ca2c291e5fb4ae0f645ce98a9c0a71501fde1894
hyeonminkim <hyeonmin@kjven0625ui-MacBookPro.local>
2022-02-09 15:50:49 +0900

    metric

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -112,7 +112,7 @@ class Angio_Dataset(torch.utils.data.Dataset):
             annotated_dot[int(y1),int(x1)]=255# y1 x1
             annotated_dot[int(y2),int(x2)]=255
 
-            annotated_dot = cv2.GaussianBlur(annotated_dot,(15,15),0)*10
+            annotated_dot = cv2.GaussianBlur(annotated_dot,(15,15),0)*30
 
             img[:,:,2] = annotated_dot
 
