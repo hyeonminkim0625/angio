6301d5367707fb8cf1421556deab72c07e91605d
hyeonminkim <hyeonmin@kjven0625ui-MacBookPro.local>
2022-02-20 18:32:43 +0900

    test

--- models/setr.py
+++ models/setr.py
@@ -21,7 +21,7 @@ class Decoder(nn.Module):
         return x
 
 class SETR(nn.Module):
-    def __init__(self, embed_dim = 256, patch_size = 8):
+    def __init__(self, embed_dim = 256, patch_size = 16):
         super(SETR, self).__init__()
         self.proj = nn.Conv2d(3, embed_dim, kernel_size=patch_size, stride=patch_size)
         self.norm_layer = nn.LayerNorm(256)
@@ -31,16 +31,16 @@ class SETR(nn.Module):
         self.num_classes = 2
         self.cls = nn.Conv2d(256,self.num_classes, 1, padding=0)
 
-        self.decoder_upscale = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.decoder_upscale = nn.Upsample(scale_factor=4, mode='bilinear')
 
         self.upscale = nn.Upsample(scale_factor=4, mode='bilinear')
 
     def forward(self, x):
         #batch channel h w
-        x = self.proj(x) + positionalencoding2d(256,64,64).unsqueeze(0).to('cuda')
+        x = self.proj(x) + positionalencoding2d(256,32,32).unsqueeze(0).to('cuda')
         x = x.flatten(2,3).permute(2,0,1)
         x = self.transformer_encoder(x)
-        x = x.permute(1,2,0).view(-1,256,64,64)
+        x = x.permute(1,2,0).view(-1,256,32,32)
 
         x = self.decoder_upscale(x)
         x = self.decoder(x)
