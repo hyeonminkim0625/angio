1764602dae275ae9a7cd670d41753eed1aa70abc
hyeonminkim <hyeonmin@kjven0625ui-MacBookPro.local>
2022-02-21 15:18:25 +0900

    vit

--- models/setr.py
+++ models/setr.py
@@ -36,7 +36,7 @@ class Decoder(nn.Module):
                                   nn.BatchNorm2d(num_features=256),
                                   nn.ReLU(),)
         """
-        self.head1 = convblock(768,256)
+        self.head1 = convblock(512,256)
         self.head2 = convblock(256,256)
         self.head3 = convblock(256,256)
         self.head4 = convblock(256,256)
@@ -54,17 +54,26 @@ class SETR(nn.Module):
         super(SETR, self).__init__()
         self.num_classes = 2
         #self.proj = nn.Conv2d(3, embed_dim, kernel_size=patch_size, stride=patch_size)
-        model = timm.create_model('vit_base_r50_s16_384', pretrained=False)
-        return_layers = {"layer1": "0", "layer2": "1", "layer3": "2", "layer4": "3"}
+        self.input_proj = nn.Conv2d(1024, 512, kernel_size=1)
+        model = timm.create_model('resnet50', pretrained=True)
+        return_layers = {"layer1": "1", "layer2": "2", "layer3": "3"}
         #self.model =  IntermediateLayerGetter(model,return_layers={'patch_embed':'0','norm':'1'})
-        self.model =  IntermediateLayerGetter(model,return_layers={'norm':'0'})
-        #transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=12)
-        self.decoder = Decoder()
-        return_layers={'stages':'0','norm':'1'}
+        self.backbone =  IntermediateLayerGetter(model,return_layers=return_layers)
+
+        encoder_layer = nn.TransformerEncoderLayer(512,8)
+        self.transformer= nn.TransformerEncoder(encoder_layer, num_layers=6)
+
+        
+        self.head1 = convblock(512,256)
+        self.head2 = convblock(768,256)
+        self.head3 = convblock(512,256)
+        self.head4 = convblock(256,256)
+
+
         self.cls = nn.Conv2d(256, self.num_classes, 1, padding = 0)
         #self.decoder_upscale = nn.Upsample(scale_factor=4, mode='nearest')
 
-        #self.upscale = nn.Upsample(scale_factor=4, mode='nearest')
+        self.upscale = nn.Upsample(scale_factor=2, mode='nearest')
 
     def forward(self, x):
         #batch channel h w
@@ -74,17 +83,27 @@ class SETR(nn.Module):
         #x = x.permute(1,2,0).view(-1,512,32,32)
 
         #x = self.decoder_upscale(x)
-        activation = {}
-        b,_,h,w = x.shape
-        x = self.model(x)['0']
+        
+        
+        xs = self.backbone(x)
+
+        xs['3'] = self.input_proj(xs['3'])
+        b,_,h,w = xs['3'].shape
+
+        xs['3'] = xs['3'] + positionalencoding2d(512,h,w).unsqueeze(0).to('cuda')
+        xs['3'] = xs['3'].flatten(2,3).permute(2,0,1)
+        xs['3'] = self.transformer(xs['3'])
+        xs['3'] = xs['3'].permute(1,2,0).view(-1,512,h,w)
         #print(x.shape)
-        x = x.transpose(2,1).reshape(b,-1,h//16,w//16)
 
         #print(x.shape)
-        x = self.decoder(x)
-        x = self.cls(x)
+        xs['3'] = self.head1(xs['3'])
+        xs['2'] = self.head2(torch.cat([xs['3'], xs['2']],dim=1))
+        xs['1'] = self.head3(torch.cat([xs['2'], xs['1']],dim=1))
+        xs['1'] = self.cls(xs['1'])
+        xs['1'] = self.upscale(xs['1'])
         #x = self.upscale(x)
-        return {"out" :x}
+        return {"out" : xs['1']}
 
 
 #main.py --batch_size=4 --model=setr --epochs=50 --lr_drop=40 --mask_argmax --opt=adamw --loss=crossentropy --lr=2e-4 --withcoordinate=concat_heatmap --img_size=384 --centerline=False --weight_path=./weight_unet_30/setr_33.pth --mode=val --saveallfig --onlymask --eval --report_hard_sample=30 --output_dir=./result --wandb
