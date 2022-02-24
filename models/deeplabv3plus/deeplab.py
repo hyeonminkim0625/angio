6b13db94ff0d7a1b28d9c2e40d60a4bfb5487b23
hyeonminkim <hyeonmin@kjven0625ui-MacBookPro.local>
2022-02-24 19:53:51 +0900

    test

--- models/deeplabv3plus/deeplab.py
+++ models/deeplabv3plus/deeplab.py
@@ -18,12 +18,12 @@ class DeepLab(nn.Module):
             BatchNorm = SynchronizedBatchNorm2d
         else:
             BatchNorm = nn.BatchNorm2d
-            
+
 
         self.backbone = build_backbone(backbone, output_stride, BatchNorm)
         self.aspp = build_aspp(backbone, 16, BatchNorm)
-        self.decoder1 = Decoder_revised(256+256,256,2)
-        self.decoder2 = Decoder_revised(256+128,256,2)
+        self.decoder1 = Decoder_revised(256+384,256,2)
+        self.decoder2 = Decoder_revised(256+192,256,2)
         
         self.cls = nn.Conv2d(256, 2, 1, padding = 0)
         self.freeze_bn = freeze_bn
