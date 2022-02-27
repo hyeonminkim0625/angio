0de24167a0ad36be68c430ac3ee03a6976be891d
hyeonminkim <hyeonmin@kjven0625ui-MacBookPro.local>
2022-02-27 16:56:10 +0900

    astrous

--- models/deeplabv3plus/aspp.py
+++ models/deeplabv3plus/aspp.py
@@ -40,7 +40,7 @@ class ASPP(nn.Module):
         elif backbone == 'hrnet':
             inplanes = 256+512+1024
         elif backbone == 'convnext':
-            inplanes = 512+1024
+            inplanes = 2304
         else:
             inplanes = 2048
         if output_stride == 16:
