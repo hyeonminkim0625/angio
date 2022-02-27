4604861441b17672e7edd57d389054dcc0f479ef
hyeonminkim <hyeonmin@kjven0625ui-MacBookPro.local>
2022-02-27 16:54:42 +0900

    astrous

--- models/deeplabv3plus/aspp.py
+++ models/deeplabv3plus/aspp.py
@@ -7,8 +7,7 @@ from models.deeplabv3plus.sync_batchnorm.batchnorm import SynchronizedBatchNorm2
 class _ASPPModule(nn.Module):
     def __init__(self, inplanes, planes, kernel_size, padding, dilation, BatchNorm):
         super(_ASPPModule, self).__init__()
-        self.atrous_conv = nn.Conv2d(inplanes, planes, kernel_size=kernel_size,
-                                            stride=1, padding=padding, dilation=dilation, bias=False)
+        self.atrous_conv = nn.Conv2d(inplanes, planes, kernel_size=kernel_size, stride=1, padding=padding, dilation=dilation, bias=False)
         self.bn = BatchNorm(planes)
         self.relu = nn.ReLU()
 
@@ -45,16 +44,16 @@ class ASPP(nn.Module):
         else:
             inplanes = 2048
         if output_stride == 16:
-            dilations = [1, 6, 12, 18]
+            dilations = [1, 2, 3, 4]
         elif output_stride == 8:
             dilations = [1, 12, 24, 36]
         else:
             raise NotImplementedError
 
-        self.aspp1 = _ASPPModule(inplanes, 256, 1, padding=0, dilation=dilations[0], BatchNorm=BatchNorm)
-        self.aspp2 = _ASPPModule(inplanes, 256, 3, padding=dilations[1], dilation=dilations[1], BatchNorm=BatchNorm)
-        self.aspp3 = _ASPPModule(inplanes, 256, 3, padding=dilations[2], dilation=dilations[2], BatchNorm=BatchNorm)
-        self.aspp4 = _ASPPModule(inplanes, 256, 3, padding=dilations[3], dilation=dilations[3], BatchNorm=BatchNorm)
+        self.aspp1 = _ASPPModule(inplanes, 256, 7, padding=dilations[0]*3, dilation=dilations[0], BatchNorm=BatchNorm)
+        self.aspp2 = _ASPPModule(inplanes, 256, 7, padding=dilations[1]*3, dilation=dilations[1], BatchNorm=BatchNorm)
+        self.aspp3 = _ASPPModule(inplanes, 256, 7, padding=dilations[2]*3, dilation=dilations[2], BatchNorm=BatchNorm)
+        self.aspp4 = _ASPPModule(inplanes, 256, 7, padding=dilations[3]*3, dilation=dilations[3], BatchNorm=BatchNorm)
 
         self.global_avg_pool = nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)),
                                              nn.Conv2d(inplanes, 256, 1, stride=1, bias=False),
