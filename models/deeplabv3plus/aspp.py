75eb0a31f82f2a711fc3d574ed7804556ad66987
Hyeonmin <hyeonmin@Hyeonminui-MacBookAir.local>
2022-03-02 00:37:16 +0900

    args

--- models/deeplabv3plus/aspp.py
+++ models/deeplabv3plus/aspp.py
@@ -78,9 +78,6 @@ class ASPP(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        print(x.shape)
-        print(self.dropout(x))
-        print(self.dropout(x).shape)
 
         return self.dropout(x)
 
