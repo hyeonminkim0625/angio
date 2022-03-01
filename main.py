ea8f0bd5404bc3400bf0dac8d13208fb38a1f845
Hyeonmin <hyeonmin@Hyeonminui-MacBookAir.local>
2022-03-01 23:59:07 +0900

    deterministic

--- main.py
+++ main.py
@@ -255,8 +255,8 @@ if __name__ == '__main__':
     random.seed(random_seed)
     if args.multigpu:
         torch.cuda.manual_seed_all(random_seed)
-    #torch.backends.cudnn.deterministic = True
-    #torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
    
     if args.output_dir:
         if args.eval:
